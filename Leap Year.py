8c5efbbe3f1c73aa27944309b0637c3a8dee119f
Bianca <149477655+inytwjo@users.noreply.github.com>
2024-01-29 14:59:35 +0800

    Update Leap Year.py

--- Leap Year.py
+++ Leap Year.py
@@ -1,6 +1,6 @@
 year=int(input("Enter a year: "))
 
-if(year % 4 == 0):
+if(year % 4 == 0): #performing leap year, it should be divisible by 4 
     print(year, " is a leap year.")
 else:
     print(year, " is not a leap year.")
