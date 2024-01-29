94bc215a3782ccec9263d5e482146b264baac389
Bianca <149477655+inytwjo@users.noreply.github.com>
2024-01-29 15:00:57 +0800

    Update Fibonacci_Series.py

--- Fibonacci_Series.py
+++ Fibonacci_Series.py
@@ -5,8 +5,9 @@ num2=1
 
 print("Here is the Fibonacci Series: ")
 
-for i in range(nterm):
-    print(num1)
+for i in range(nterm): #looping until nterm 
+    print(num1) #displaying first value as 0
+    #fibonacci calculation: 
     newnum = num1 + num2
     num1 = num2
     num2 = newnum
